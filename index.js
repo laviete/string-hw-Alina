81fc67c2ce063c04e11dfc618d87ef5cf21feacd
Alina <135998117+laviete@users.noreply.github.com>
2023-07-28 02:13:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-const getNumVowelsIn = (str) => {
+const getNumVowels = (str) => {
   const vowels = ["A", "E", "I", "O", "U", "Y", "a", "e", "i", "o", "u", "y"];
   return str.split("").filter((letter) => vowels.includes(letter)).length;
 };
@@ -7,3 +7,4 @@ console.log(getNumVowels("console")); // 3
 console.log(getNumVowels("www")); // 0
 console.log(getNumVowels("oOo!")); // 3
 console.log(getNumVowels("QwertY")); // 2
+
